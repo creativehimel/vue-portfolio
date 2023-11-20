496b95cf15aa32a8fcc8ed4d099b78a53a289309
creativehimel <himel.phy@bsmrstu.edu.bd>
2023-11-20 21:37:27 +0600

    Project Initialized

---
+++ src/components/Hero.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+<template>
+  <section class="bg-white dark:bg-gray-900">
+    <div class="container mx-auto flex justify-between items-center">
+      <div>
+        <h1
+          class="text-5xl font-bold leading-none text-gray-800 dark:text-white"
+        >
+          Flowbite
+        </h1>
+      </div>
+      <div>
+        <img src="../assets/hero.gif" alt="" class="w-80" />
+      </div>
+    </div>
+  </section>
+</template>
+<style scoped></style>
